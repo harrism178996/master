befbd1e58568144ebb35c61d6f80f71edf1f9a3e
harrism178996 <52997912+harrism178996@users.noreply.github.com>
2023-07-18 15:26:12 -0400

    Update ugit.py

--- ugit.py
+++ ugit.py
@@ -19,16 +19,16 @@ global internal_tree
 #### -------------User Variables----------------####
 #### 
 # Default Network to connect using wificonnect()
-ssid = "OpenMuscle"
-password = "3141592653"
+ssid = "AAM IoT"
+password = "AAMp0wer"
 
 # CHANGE TO YOUR REPOSITORY INFO
 # Repository must be public if no personal access token is supplied
-user = 'turfptax'
-repository = 'ugit_test'
+user = 'harrism178996'
+repository = 'master.git'
 token = ''
 # Change this variable to 'master' or any other name matching your default branch
-default_branch = 'main'
+default_branch = 'master'
 
 # Don't remove ugit.py from the ignore_files unless you know what you are doing :D
 # Put the files you don't want deleted or updated here use '/filename.ext'
