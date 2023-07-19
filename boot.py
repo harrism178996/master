bb9dba1dee54e64e126379ec34bbf82803bc5eb2
harrism178996 <52997912+harrism178996@users.noreply.github.com>
2023-07-19 12:47:31 -0400

    Update boot.py

--- boot.py
+++ boot.py
@@ -5,12 +5,13 @@ import machine
 import time
 import ugit
 
-led = machine.Pin(15, machine.Pin.Out)
-for i in range(10)
-led.value(1)
-time.sleep(0.5)
-led.value(0)
-time.sleep(0.5)
+led = Pin(2, Pin.OUT)
+for i in range(10):
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
+
 
 
 print('hello ugit users!')
